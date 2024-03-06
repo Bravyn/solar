3fa2e2f0a27fd50b90e05148f1398edf097e2a19
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-06 08:33:18 +0300

    added  both home and about views

--- stars/urls.py
+++ stars/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('stars/', views.star, name = 'star'),
+    path('about/', views.about, name = "about"),
+    path('/', views.home, name= "home")
 
 ]
--- stars/views.py
+++ stars/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.template import loader
 
 # Create your views here.
-def star(request):
+def star(request: HttpRequest ):
     template = loader.get_template("stars.html")
     return HttpResponse(template.render())
+
+def about(request: HttpRequest ):
+    template2 = loader.get_template("about.html")
+    return HttpResponse(template2.render())
+    
+def home():
+    home_template = loader.get_template("home.html")
+    return HttpResponse(home_template.render())
