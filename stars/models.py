41356c1e81fd275025faced8193d06f8381e1a26
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-11 11:28:14 +0300

    changed from members model to StarDetails model namer

--- stars/models.py
+++ stars/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Member(models.Model):
-    firsname = models.CharField(max_length = 255)
-    lastname = models.CharField(max_length = 255)
+class StarDetails(models.Model):
+    name = models.CharField(max_length = 255)
+    surface_temperature = models.CharField(max_length = 255)
+
     
