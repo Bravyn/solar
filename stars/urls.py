0b8b59baf92a89addd0a910008b8d300582be6d4
Ian Bravyn <ianbravynsa@gmail.com>
2024-03-13 17:28:39 +0300

    .

--- stars/urls.py
+++ stars/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.star, name = "star"),
     path('home/about/', views.about, name = "about"),
-    path('stars/home/', views.home, name= "home"),
-    path('stars/home/star_details/', views.star_details, name= "star_details")
+    path('stars/', views.home, name= "home"),
+    path('stars/star_details/', views.star_details, name= "star_details")
 
 ]
